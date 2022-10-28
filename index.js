5add07c6d4d68334b85419afff8c75d34077a91f
unknown <tasnimulislamshadi@gmail.com>
2022-10-28 20:16:23 +0600

    course and lesson api created

--- index.js
+++ index.js
@@ -18,6 +18,36 @@ app.get('/courses', (req, res) => {
     res.send(courses)
 });
 
+//Api >> courses by category
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '01') {
+        res.send(courses)
+    }
+    else {
+        const selectedCourses = courses.filter(courses => courses.category_id === id);
+        res.send(selectedCourses);
+    }
+
+});
+
+//Api >> single course by id
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selected = courses.find(course => course.id === id);
+    res.send(selected);
+});
+
+
+//Api >> single lesson by id
+app.get('/course/:course_id/lesson/:lesson_id', (req, res) => {
+    const course_id = req.params.course_id;
+    const les_id = req.params.lesson_id;
+    const course = courses.find(course => course.id === course_id);
+    const lesson = course.lessons.find(les => les.lesson_id === les_id);
+    // console.log(lesson);
+    res.send(lesson);
+});
 
 
 // run server
